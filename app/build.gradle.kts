7f87d83834a3d9a3f855aca3677a8dfffa0c72e2
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2023-11-14 16:59:32 -0500

    tercer commit: 17:00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.examen01_carrera_guanoluisa"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.examen01_carrera_guanoluisa"
--- app/src/main/java/com/example/examen01_carrera_guanoluisa/Suma.java
+++ app/src/main/java/com/example/examen01_carrera_guanoluisa/Suma.java
@@ -8,9 +8,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.math.BigInteger;
+
 public class Suma extends AppCompatActivity {
 
     EditText num1, num2;
+    TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +21,18 @@ public class Suma extends AppCompatActivity {
 
         num1 = findViewById(R.id.num1);
         num2 = findViewById(R.id.num2);
+        resultado = findViewById(R.id.Resultado);
     }
 
     public void suma(View view){
+        String num1String = num1.getText().toString();
+        String num2String = num2.getText().toString();
 
+        BigInteger bigInteger1 = new BigInteger(num1String);
+        BigInteger bigInteger2 = new BigInteger(num2String);
+
+        BigInteger resultadoSuma = bigInteger1.add(bigInteger2);
+
+        resultado.setText("Resultado: " + resultadoSuma.toString());
     }
 }
